dda0a0a1a5192bf134ce6c25e361038712b18b83
Luke <lukedam@gmail.com>
2019-11-27 11:19:37 -0500

    Initial Commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Xcode User on 2019-11-27.
+//  Copyright © 2019 sheridan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hello()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func hello() {
+        
+    }
+
+
+}
+
