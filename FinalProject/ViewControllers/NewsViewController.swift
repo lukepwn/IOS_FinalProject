f03fa8bf5d1a620beb5e63b4e102d173f044aaa2
Trevor Rubie <rubiet@sheridancollege.ca>
2019-11-29 15:19:22 -0500

    news viewcontroller

---
+++ FinalProject/ViewControllers/NewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsViewController.swift
+//  FinalProject
+//
+//  Created by Trevor Rubie on 2019-11-29.
+//  Copyright © 2019 sheridan. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
